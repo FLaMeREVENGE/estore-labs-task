22a8d1fb89074a1050128a573f8069ba71c0863d
Karol Komorowski <96321074+FLaMeREVENGE@users.noreply.github.com>
2024-01-10 13:44:34 +0100

    drag works

--- src/components/FormProduct.jsx
+++ src/components/FormProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import { z, object, string } from "zod";
@@ -47,34 +47,38 @@ const FormProduct = () => {
   });
 
   const [newPoint, setNewPoint] = useState("");
-  const [points, setPoints] = useState([]);
+  const [points, setPoints] = useState({ list: [], counter: 0 });
 
   const onDragEnd = (result) => {
     if (!result.destination) return;
     const { source, destination } = result;
-    const pointsCopy = [...points];
+    const pointsCopy = [...points.list];
     const [movedItem] = pointsCopy.splice(source.index, 1);
     pointsCopy.splice(destination.index, 0, movedItem);
-    setPoints(pointsCopy);
+    setPoints({ list: pointsCopy, counter: points.counter + 1 });
     setValue("points", pointsCopy);
   };
 
   const handleAddPoint = () => {
     if (newPoint.trim() !== "") {
-      const pointsCopy = [...points];
+      const pointsCopy = [...points.list];
       pointsCopy.push(newPoint);
-      setPoints(pointsCopy);
+      setPoints({ list: pointsCopy, counter: points.counter + 1 });
       setValue("points", pointsCopy);
       setNewPoint("");
     }
   };
 
   const handleRemovePoint = (indexToRemove) => {
-    const pointsCopy = [...points];
+    const pointsCopy = [...points.list];
     pointsCopy.splice(indexToRemove, 1);
-    setPoints(pointsCopy);
+    setPoints({ list: pointsCopy, counter: points.counter + 1 });
     setValue("points", pointsCopy);
   };
+
+  useEffect(() => {
+    // Tutaj możesz dodać dowolny kod, który chcesz wykonać po zmianie kolejności elementów.
+  }, [points.counter]);
 
   const onSubmit = (data) => {
     console.log(data);
@@ -89,9 +93,7 @@ const FormProduct = () => {
           control={control}
           render={({ field }) => <Form.Control {...field} type="text" />}
         />
-        {errors.title && (
-          <p className="error-text label">Title is required</p>
-        )}
+        {errors.title && <p className="error-text label">Title is required</p>}
       </Form.Group>
       <Form.Group>
         <Form.Label className="label">Description:</Form.Label>
@@ -116,64 +118,82 @@ const FormProduct = () => {
         />
       </Form.Group>
       <Form.Group>
-  <Form.Label className="label">Bullets:</Form.Label>
-  <DragDropContext onDragEnd={onDragEnd}>
-    {points.map((point, index) => (
-      <Droppable key={`droppable-${index}`} droppableId={`droppable-${index}`}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            <Draggable key={`draggable-${index}`} draggableId={`draggable-${index}`} index={index}>
+        <Form.Label className="label">Bullets:</Form.Label>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {points.list.map((point, index) => (
+            <Droppable
+              key={`droppable-${index}`}
+              droppableId={`droppable-${index}`}
+            >
               {(provided) => (
-                <div ref={provided.innerRef} {...provided.draggableProps}>
-                  <div className="d-flex">
-                    <Controller
-                      name={`points[${index}]`}
-                      control={control}
-                      defaultValue={point}
-                      render={({ field }) => (
-                        <Form.Control
-                          {...field}
-                          type="text"
-                          className="mr-2"
-                        />
-                      )}
-                    />
-                    <Button
-                      variant="danger"
-                      onClick={() => handleRemovePoint(index)}
-                    >
-                      Remove
-                    </Button>
-                    <div
-                      className="drag-handle"
-                      {...provided.dragHandleProps}
-                    >
-                      <FontAwesomeIcon icon={faBars} />
-                    </div>
-                  </div>
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  <Draggable
+                    key={`draggable-${point}`}
+                    draggableId={`draggable-${point}`}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.draggableProps}>
+                        <div className="d-flex">
+                          <div
+                            className="drag-handle"
+                            {...provided.dragHandleProps}
+                          >
+                            <FontAwesomeIcon icon={faBars} />
+                          </div>
+                          <Controller
+                            name={`points[${index}]`}
+                            control={control}
+                            value={point}
+                            render={({ field }) => (
+                              <Form.Control
+                                {...field}
+                                type="text"
+                                className="mr-2"
+                                onChange={(e) => {
+                                  field.onChange(e);
+                                  const updatedPoints = [...points.list];
+                                  updatedPoints[index] = e.target.value;
+                                  setPoints({
+                                    list: updatedPoints,
+                                    counter: points.counter + 1,
+                                  });
+                                  setValue("points", updatedPoints);
+                                }}
+                              />
+                            )}
+                          />
+                          <Button
+                            variant="danger"
+                            onClick={() => handleRemovePoint(index)}
+                          >
+                            Remove
+                          </Button>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                  {provided.placeholder}
                 </div>
               )}
-            </Draggable>
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    ))}
-  </DragDropContext>
-  <div className="mt-2">
-    <Form.Control
-      type="text"
-      value={newPoint}
-      onChange={(e) => setNewPoint(e.target.value)}
-      placeholder="New point..."
-    />
-    <Button variant="primary" className="mt-2" onClick={handleAddPoint}>
-      Add Point
-    </Button>
-  </div>
-</Form.Group>
-
-      <Button type="submit">Send</Button>
+            </Droppable>
+          ))}
+        </DragDropContext>
+        <div className="mt-2">
+          <Form.Control
+            type="text"
+            value={newPoint}
+            onChange={(e) => setNewPoint(e.target.value)}
+            placeholder="New point..."
+          />
+          <Button variant="primary" className="mt-2" onClick={handleAddPoint}>
+            Add Point
+          </Button>
+        </div>
+      </Form.Group>
+      <Button className="w-100 mt-3" type="submit">
+        Send
+      </Button>
     </Form>
   );
 };
