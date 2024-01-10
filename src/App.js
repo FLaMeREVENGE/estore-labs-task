92c15cc23a9b9eb4bad77b03a44a7e8ab5143535
Karol Komorowski <96321074+FLaMeREVENGE@users.noreply.github.com>
2024-01-10 11:54:07 +0100

    zod implement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./scss/index.scss";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Title from "./components/Title";
 import FormProduct from "./components/FormProduct";
 
--- src/components/FormProduct.jsx
+++ src/components/FormProduct.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { Form, Button } from 'react-bootstrap';
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Form, Button } from "react-bootstrap";
+import { object, string } from "zod";
+
+const schema = object({
+  title: string().min(1).max(100),
+  description: string().min(1).max(500).optional(),
+  points: string().min(1).max(500).optional(),
+  keywords: string().min(1).max(100).optional(),
+});
+
+const defaultValues = {
+  title: "",
+  description: "",
+  points: "",
+  keywords: "",
+};
 
 const FormProduct = () => {
-  const { handleSubmit, control } = useForm();
+  const { handleSubmit, control, formState: { errors } } = useForm({
+    defaultValues,
+    resolver: (data) => {
+      try {
+        schema.parse(data);
+        return {
+          values: data,
+          errors: {},
+        };
+      } catch (err) {
+        return {
+          values: {},
+          errors: err.formErrors.fieldErrors,
+        };
+      }
+    },
+  });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -12,16 +43,18 @@ const FormProduct = () => {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Form.Group>
-        <Form.Label className='label'>Title:</Form.Label>
+        <Form.Label className="label">Title:</Form.Label>
         <Controller
-        
           name="title"
           control={control}
-          render={({ field }) => <Form.Control {...field} type="text" />}
+          render={({ field }) => (
+            <Form.Control {...field} type="text" />
+          )}
         />
+        {errors.title && <p className="error-text label">Tytuł jest wymagany.</p>}
       </Form.Group>
       <Form.Group>
-        <Form.Label className='label'>Description:</Form.Label>
+        <Form.Label className="label">Description:</Form.Label>
         <Controller
           name="description"
           control={control}
@@ -29,7 +62,7 @@ const FormProduct = () => {
         />
       </Form.Group>
       <Form.Group>
-        <Form.Label className='label'>Bullets:</Form.Label>
+        <Form.Label className="label">Bullets:</Form.Label>
         <Controller
           name="points"
           control={control}
@@ -37,7 +70,7 @@ const FormProduct = () => {
         />
       </Form.Group>
       <Form.Group>
-        <Form.Label className='label'>Keywords:</Form.Label>
+        <Form.Label className="label">Keywords:</Form.Label>
         <Controller
           name="keywords"
           control={control}
